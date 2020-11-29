ae9cd32e3d2716b69e87769266e9360a9c95ff34
tripleji <67589423+tripleji@users.noreply.github.com>
2020-11-29 21:55:49 +0900

    Complete the Dark Mode

--- Web 3/script.js
+++ Web 3/script.js
@@ -115,9 +115,13 @@ xBtn.addEventListener("click", () => {
 
 const darkElements1= document.querySelectorAll(".dark-mode-1");
 const darkElements2= document.querySelectorAll(".dark-mode-2");
+const lightTexts = document.querySelectorAll(".light-text");
+const borders = document.querySelectorAll(".border");
 
 toggle.addEventListener("click", () => {
   circle.classList.toggle("move");
   Array.from(darkElements1).map((darkEl1) => darkEl1.classList.toggle("dark-1"));
   Array.from(darkElements2).map((darkEl2) => darkEl2.classList.toggle("dark-2"));
+  Array.from(lightTexts).map((lightText) => lightText.classList.toggle("light"));
+  Array.from(borders).map((border) => border.classList.toggle("border-colorr"));
 });
