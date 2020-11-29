3d95486dac5213e305d0d78f51ae071165be21d5
tripleji <67589423+tripleji@users.noreply.github.com>
2020-11-29 21:35:00 +0900

    Dark Mode 50%

--- Web 3/script.js
+++ Web 3/script.js
@@ -112,6 +112,12 @@ xBtn.addEventListener("click", () => {
 });
 
 // Dark Mode
+
+const darkElements1= document.querySelectorAll(".dark-mode-1");
+const darkElements2= document.querySelectorAll(".dark-mode-2");
+
 toggle.addEventListener("click", () => {
   circle.classList.toggle("move");
+  Array.from(darkElements1).map((darkEl1) => darkEl1.classList.toggle("dark-1"));
+  Array.from(darkElements2).map((darkEl2) => darkEl2.classList.toggle("dark-2"));
 });
