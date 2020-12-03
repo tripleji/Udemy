02ca6411cf5c190f4186206a7eaa09076f926090
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-03 20:56:30 +0900

    Update script.js

--- JavaScript 1/script.js
+++ JavaScript 1/script.js
@@ -1,3 +1,56 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function (){
+   const guess = Number(document.querySelector('.guess').value);
+   console.log(guess, typeof guess);
+
+//숫자 입력
+   if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+
+//승리할 경우    
+   } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+
+
+   } else if (guess > secretNumber) {
+    if (score > 1) {
+        document.querySelector('.message').textContent = 'Too high!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else {
+        document.querySelector('.message').textContent = 'You lost the game!!';
+        document.querySelector('.score').textContent = 0;
+    } 
+    
+    }else if (guess < secretNumber) {
+    if (score > 1) {
+        document.querySelector('.message').textContent = 'Too low!';
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else {
+        document.querySelector('.message').textContent = 'You lost the game!!';
+        document.querySelector('.score').textContent = 0;
+    }
+    }
+});
