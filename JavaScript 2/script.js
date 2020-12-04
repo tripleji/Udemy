731b1431adf54aeabffc22b827fa0fc94ca32bf4
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-04 09:52:02 +0900

    Complete the JavaScript 2

--- JavaScript 2/script.js
+++ JavaScript 2/script.js
@@ -5,7 +5,29 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal= document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const openModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
 
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// 팝업 열기
 for(let i = 0; i < btnOpenModal.length; i++) 
-console.log(btnOpenModal [i].textContent);
+btnOpenModal [i].addEventListener('click', openModal);
+
+
+// 팝업 닫기
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    console.log(e.key);
+
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+            closeModal();
+        }
+});
