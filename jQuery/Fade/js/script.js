e5462e3c16d28e5318f58d0c539a352f4ff04ba0
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-11 14:32:32 +0900

    Practice

--- jQuery/Fade/js/script.js
+++ jQuery/Fade/js/script.js
@@ -1,3 +1,8 @@
 $(function () {
-  $(".lightbox").delay(500).fadeIn(1000);
+  // $("p").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("input[type="text"]").css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8)");
 });
+   
