a6b1d9b4ad2fdf63143297bf86a0ef135275e10a
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-14 01:31:03 +0900

    Operator practice

---
+++ TypeScript/note/5_operator.ts
@@ -0,0 +1,11 @@
+// function logMessage(value: any){
+//     console.log(value);
+// }
+// logMessage('hello');
+// logMessage(100);
+
+function logMessage(value: string | number){
+    console.log(value);
+}
+logMessage('hello');
+logMessage(100);
