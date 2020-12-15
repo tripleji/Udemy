4a9efd4609fa137251e1d31e66c8b39dd6612843
tripleji <67589423+tripleji@users.noreply.github.com>
2020-12-15 23:01:26 +0900

    Update 5_operator.ts

--- TypeScript/note/5_operator.ts
+++ TypeScript/note/5_operator.ts
@@ -4,8 +4,27 @@
 // logMessage('hello');
 // logMessage(100);
 
+var gogo: string | number | boolean;
+
 function logMessage(value: string | number){
     console.log(value);
+    if (typeof value === 'number'){
+        value.toLocaleString();
+}
+if (typeof value === 'string'){
+    value.toString();
+}
+throw new TypeError('value must be string or number');
 }
 logMessage('hello');
 logMessage(100);
+
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface People {
+    name: string;
+    age: number;
+}
